4d861758cf52a0a0ca08089897984dd77fe290d7
Frank S. Thomas <frank@timepit.eu>
2012-07-25 22:10:10 +0200

    Use only legal chars for XML texts.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,7 @@
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
+import Test.QuickCheck
 import Test.QuickCheck.Arbitrary
 
 import Account
@@ -18,8 +19,15 @@ tests = [
 
 prop_p a = (readXmlString (showXmlString a) :: Maybe Account) == Just a
 
+-- http://www.w3.org/TR/REC-xml/#charsets
+legalXmlChars :: String
+legalXmlChars = ['\x9', '\xA', '\xD']
+             ++ ['\x20' .. '\xD7FF']
+             ++ ['\xE000' .. '\xFFFD']
+             -- ++ ['\x10000' .. '\x10FFFF']
+
 instance Arbitrary Account where
   arbitrary = do
-    x <- arbitrary
-    y <- arbitrary
-    return (Account x y)
+    id <- listOf $ elements legalXmlChars
+    name <- listOf $ elements legalXmlChars
+    return (Account id name)
