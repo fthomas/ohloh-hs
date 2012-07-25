d502bfad1248463c8b5ef1e139b3525194243575
Frank S. Thomas <frank@timepit.eu>
2012-07-25 20:31:47 +0200

    Added uncurry5.

--- src/Common.hs
+++ src/Common.hs
@@ -12,3 +12,6 @@ class ReadXmlString a where
 class ShowXmlString a where
   showXmlString :: (XmlPickler a) => a -> String
   showXmlString = showPickled []
+
+uncurry5 :: (t1 -> t2 -> t3 -> t4 -> t5 -> t) -> (t1, t2, t3, t4, t5) -> t
+uncurry5 f (x1, x2, x3, x4, x5) = f x1 x2 x3 x4 x5
