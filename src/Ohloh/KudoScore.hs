c6c5f0ce2fbd653bcc459b2a2648d3fd45328bbe
Frank S. Thomas <frank@timepit.eu>
2012-07-28 13:31:16 +0200

    Added license headers.

--- src/Ohloh/KudoScore.hs
+++ src/Ohloh/KudoScore.hs
@@ -1,3 +1,9 @@
+-- Copyright © 2012 Frank S. Thomas <frank@timepit.eu>
+-- All rights reserved.
+--
+-- Use of this source code is governed by a BSD-style license that
+-- can be found in the LICENSE file.
+
 module Ohloh.KudoScore (
   KudoScore(..),
   readXmlString,
