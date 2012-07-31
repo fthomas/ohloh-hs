606f8c570b7665da0a1402d624997ae2e69fdfb2
Frank S. Thomas <frank@timepit.eu>
2012-07-31 21:13:43 +0200

    Stylistic changes.

--- tests/Main.hs
+++ tests/Main.hs
@@ -4,10 +4,8 @@
 -- Use of this source code is governed by a BSD-style license that
 -- can be found in the LICENSE file.
 
-{-#
-OPTIONS -fno-warn-missing-signatures
-        -fno-warn-orphans
-#-}
+{-# OPTIONS -fno-warn-missing-signatures
+            -fno-warn-orphans #-}
 
 import Test.QuickCheck
 import Test.Framework (defaultMain, testGroup)
