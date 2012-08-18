47f7c388103a23925711a46b602b2e277f913502
Frank S. Thomas <frank@timepit.eu>
2012-08-18 08:43:25 +0200

    Added links to Ohloh's reference.

--- src/Ohloh/Stack.hs
+++ src/Ohloh/Stack.hs
@@ -4,6 +4,7 @@
 -- Use of this source code is governed by a BSD-style license that
 -- can be found in the LICENSE file.
 
+-- | <http://meta.ohloh.net/referencestack/>
 module Ohloh.Stack (
   Stack(..),
   xpStack
