2a5460d72fc740567b80a561d203065d80d1c685
Frank S. Thomas <frank@timepit.eu>
2012-10-08 07:35:12 +0200

    Download more queries.

--- tests/queries/update.hs
+++ tests/queries/update.hs
@@ -1,5 +1,4 @@
 import qualified Data.ByteString as BS
-import Data.Maybe
 import Network.Curl.Download
 
 import Ohloh
@@ -7,16 +6,23 @@ import Ohloh
 key = ""
 
 downloadFile item args = do
-  let url = fromJust $ queryUrl item args `withApiKey` key
+  let Just url = queryUrl item args `withApiKey` key
+  let filename = item ++ ".xml"
+
+  putStrLn $ "Downloading " ++ url
   content <- openURI url
   case content of
     Left  x -> putStrLn x
-    Right x -> BS.writeFile (item ++ ".xml") x
+    Right x -> BS.writeFile filename x
 
 main = do
   downloadFile "account" ["fst"]
   downloadFile "activity_facts" ["ohloh-hs", "latest"]
   downloadFile "analysis" ["ohloh-hs", "latest"]
+  downloadFile "contributor_facts" ["ohloh-hs"]
+  downloadFile "enlistments" ["ohloh-hs"]
+  downloadFile "factoids" ["ohloh-hs"]
   downloadFile "language" ["haskell"]
   downloadFile "project" ["ohloh-hs"]
   downloadFile "size_facts" ["ohloh-hs", "latest"]
+  downloadFile "stacks" ["ghc"]
